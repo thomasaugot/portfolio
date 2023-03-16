120d90812f707aeaff328e9f96729c0555823a41
Thomas Augot <thomas.augot@hotmail.fr>
2023-03-16 16:36:13 +0100

    adding spinner loading

--- src/App.js
+++ src/App.js
@@ -6,19 +6,36 @@ import Header from "./components/Header/Header";
 import HomePage from "./components/Home/HomePage";
 import PortfolioPage from "./components/Portfolio/PortfolioPage";
 import { Fade } from "react-awesome-reveal";
+import Loading from "../src/components/Loading/Loading";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2500);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-      <Header />
-      <Fade duration={2000}>
-        <HomePage />
-        <AboutPage />
-        <CurriculumPage />
-        <PortfolioPage />
-        <ContactPage />
-        <Footer />
-      </Fade>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <Header />
+          <HomePage />
+          <Fade duration={2000}>
+            <AboutPage />
+            <CurriculumPage />
+            <PortfolioPage />
+            <ContactPage />
+            <Footer />
+          </Fade>
+        </>
+      )}
     </div>
   );
 }
