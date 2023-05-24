61b1490afe3249b518c38e26cd7b728ccb0b712d
Thomas Augot <thomas.augot@hotmail.fr>
2023-05-24 14:54:37 +0200

    fixing animation glitch

--- src/components/About/AboutPage.js
+++ src/components/About/AboutPage.js
@@ -1,20 +1,28 @@
-import "./AboutPage.scss";
+import React from "react";
 import { Slide } from "react-awesome-reveal";
+import { useInView } from "react-intersection-observer";
+import profileImage from "./profile.png";
+import "./AboutPage.scss";
 
 function AboutPage() {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.8,
+  });
+
   return (
-    <div className="AboutPage" id="AboutPage">
-      <Slide direction={"down"} delay={100} triggerOnce={true} fraction={1}>
+    <div className={`AboutPage ${inView ? "visible" : ""}`} ref={ref} id="AboutPage">
+      <Slide direction="down" triggerOnce={true} fraction={1} delay={100}>
         <h1 style={{ marginTop: "2vh" }}>About me&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h1>
       </Slide>
-      <br></br>
+      <br />
       <div className="about__content">
-        <Slide direction={"down"} triggerOnce={true} fraction={1}>
+        <Slide direction="down" triggerOnce={true} fraction={1}>
           <p>
             I am a French web developer with a specialization in React, currently based on the
             beautiful island of Fuerteventura, where the summer never ends. After 10 years of
             travelling around the world, I found myself a passion for coding and quit my old job to
-            enrol in a web development Bootcamp.
+            enroll in a web development Bootcamp.
             <br />
             <br />
             Although I spent most of my professional career in the hospitality industry, which
@@ -31,8 +39,8 @@ function AboutPage() {
             my code.
           </p>
         </Slide>
-        <Slide direction={"up"} triggerOnce={true} fraction={1}>
-          <img src={require("./profile.png")} alt="profile" className="profilePicture" />
+        <Slide direction="up" triggerOnce={true} fraction={1}>
+          <img src={profileImage} alt="profile" className="profilePicture" />
         </Slide>
       </div>
     </div>
