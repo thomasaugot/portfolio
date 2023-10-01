16a08cf56178ea0193ec8ee0bdd2d906b81a58a0
Thomas Augot <thomas.augot@hotmail.fr>
2023-10-01 23:34:49 +0200

    updating transltion and searchbar logic

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -10,7 +10,7 @@ import FooterArticles from "../../components/Footer/FooterArticles";
 const BlogPage = ({ articles }) => {
   const [setIsVisible] = useState(false);
   const { inView } = useInView({
-    threshold: 0.2, // set threshold to 20%
+    threshold: 0.2,
   });
   const [searchTerm, setSearchTerm] = useState("");
 
@@ -23,26 +23,26 @@ const BlogPage = ({ articles }) => {
   const { t } = useTranslation();
 
   const filteredArticles = articles?.filter((item) => {
-    return item?.title?.toLowerCase().includes(searchTerm.toLowerCase());
-  });
+    const searchTermLower = searchTerm.toLowerCase();
+    const titleLower = item?.title?.toLowerCase();
+    const tagsLower = item?.tags?.map((tag) => tag.toLowerCase());
 
-  console.log("Filtered articles:", filteredArticles);
+    return (
+      titleLower.includes(searchTermLower) ||
+      (tagsLower && tagsLower.some((tag) => tag.includes(searchTermLower)))
+    );
+  });
 
   return (
     <>
       <div className="BlogPage">
         <HeaderBlogMain />
         <div className="top-content">
-          <h1 className="blog-title gradient-underline-blog">React & Roll: Tom's dev blog</h1>
+          <h1 className="blog-title gradient-underline-blog">Web Dev for Everyone</h1>
           <div className="blog-intro">
             <p>
               {t(
-                "Like many before me, my short career as a full-stack web and mobile developer gave me the opportunity to do a lot of research in order to complete both personal and professional projects. I therefore decided to share some of the results of this research and offer articles aimed at documenting on various subjects related to development in general. Happy reading!"
-              )}
-            </p>
-            <p>
-              {t(
-                "NB: All articles are written in English to satisfy as many people as possible :)"
+                "Like many before me, my little career as a full-stack web and mobile developer gave me the opportunity to do a lot of research in order to complete both personal and professional projects. Focusing on Javascript and its frameworks such as React and react native, I decided to share some of the results of this research and offer articles aimed at documenting on various subjects related to development in general. Happy reading!"
               )}
             </p>
           </div>
