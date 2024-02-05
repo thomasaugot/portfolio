7229c5a0b505f281c105da56d163b6100e703102
Thomas Augot <thomas.augot@hotmail.fr>
2024-02-05 21:12:12 +0100

    update title animation

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,65 +1,80 @@
 import "./HomePage.scss";
+import React, { useState, useCallback, useRef, useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import { motion } from "framer-motion";
+import { useTransition, animated } from "@react-spring/web";
 
 function HomePage() {
   const { t } = useTranslation();
+  const ref = useRef([]);
+  const [items, set] = useState([]);
+  const transitions = useTransition(items, {
+    from: {
+      opacity: 0,
+      height: 0,
+      innerHeight: 0,
+      transform: "perspective(600px) rotateX(0deg)",
+    },
+    enter: [
+      { opacity: 1, height: 80, innerHeight: 80 },
+      { transform: "perspective(600px) rotateX(180deg)" },
+      { transform: "perspective(600px) rotateX(0deg)" },
+    ],
+    leave: [{ innerHeight: 0 }, { opacity: 0, height: 0 }],
+  });
+
+  const reset = useCallback(() => {
+    ref.current.forEach(clearTimeout);
+    ref.current = [];
+    set([]);
+    ref.current.push(
+      setTimeout(
+        () =>
+          set([
+            t("Hi! I am Thomas,"),
+            t("Full-Stack Developer"),
+            t("I bring your project to life using the latest development technologies"),
+          ]),
+        2000
+      )
+    );
+    ref.current.push(
+      setTimeout(
+        () =>
+          set([
+            t("Hi! I am Thomas,"),
+            t("I bring your project to life using the latest development technologies"),
+          ]),
+        5000
+      )
+    );
+    ref.current.push(
+      setTimeout(
+        () =>
+          set([
+            t("Hi! I am Thomas,"),
+            t("Full-Stack Developer"),
+            t("I bring your project to life using the latest development technologies"),
+          ]),
+        8000
+      )
+    );
+  }, [t]);
+
+  useEffect(() => {
+    reset();
+    return () => ref.current.forEach(clearTimeout);
+  }, [reset]);
 
   return (
     <div className="Homepage" id="HomePage">
-      <div className="topContent">
-        <div className="titleDiv">
-          <motion.div
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            initial={{ x: "-100%", opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{
-              type: "spring",
-              stiffness: 40,
-              delay: 0.4,
-              ease: "easeOut",
-            }}
-          >
-            <h1 className="mainTitle gradient-text" style={{ textDecoration: "none" }}>
-              {t("Hi! I am Thomas,")}
-            </h1>
-          </motion.div>
-          <motion.div
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            initial={{ x: "-100%", opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{
-              type: "spring",
-              stiffness: 40,
-              delay: 0.7,
-              ease: "easeOut",
-            }}
-          >
-            <h1 className="mainTitle gradient-text" style={{ textDecoration: "none" }}>
-              {t("Full-Stack Developer")}
-            </h1>
-          </motion.div>
-          <motion.div
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            initial={{ x: "-100%", opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{
-              type: "spring",
-              stiffness: 40,
-              delay: 1.0,
-              ease: "easeOut",
-            }}
-          >
-            <p className="homepageP">
-              {t("I bring your project to life using the latest development technologies")}
-            </p>
-          </motion.div>
-        </div>
-
-        <br></br>
+      <div className="title-block">
+        {transitions(({ innerHeight, ...rest }, item) => (
+          <animated.div style={rest} onClick={reset} className="title-item gradient-text">
+            <animated.div style={{ overflow: "hidden", height: "auto" }} className="gradient-text">
+              {item}
+            </animated.div>
+          </animated.div>
+        ))}
       </div>
     </div>
   );
