a307bbcb5b69300af3ba7ae6150284e8c0d087ca
Thomas Augot <thomas.augot@hotmail.fr>
2024-04-04 16:00:29 +0200

    fixing env variables exposure

--- src/api/supabase.js
+++ src/api/supabase.js
@@ -1,8 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
+import { env } from "dotenv";
 
-const supabaseUrl = "https://wshpvyskclbbggfhpkxv.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndzaHB2eXNrY2xiYmdnZmhwa3h2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU2NDY4MTAsImV4cCI6MjAxMTIyMjgxMH0.jHn7NB_0TCA1gw3skW2lZ2fca0WRdZ4FkE2VSgOr_Iw";
+env();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
